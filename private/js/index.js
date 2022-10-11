b7e7bcaa9a51fa26ec21b6ad5381dc279ff94f54
jwcub <jwcub@outlook.com>
2022-10-11 21:45:11 +0800

    移除盗版fa

--- private/js/index.js
+++ private/js/index.js
@@ -1,19 +1,3 @@
 $(() => {
     $(".ui.sidebar").sidebar("attach events", ".toc.item");
-
-    const vditor = new Vditor("editor", {
-        height: 170,
-        toolbarConfig: {
-            hide: true
-        },
-        resize: {
-            enable: true
-        },
-        tab: "    ",
-        preview: {
-            math: {
-                inlineDigit: true
-            }
-        }
-    });
 });
